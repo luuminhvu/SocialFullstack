9ee5b92764928803570a2661d1c8615cfd9b2268
Luu Vu <vu7178@gmail.com>
2023-07-06 22:42:23 +0700

    Server Done

--- server/index.js
+++ server/index.js
@@ -11,10 +11,13 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
 import { register } from './controllers/auth.js';
-import { createPost } from './controllers/post.js';
+import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
 import usersRoutes from './routes/users.js';
-import postRoutes from './routes/post.js';
+import postRoutes from './routes/posts.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 /*config*/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -51,5 +54,7 @@ mongoose
     .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+        // User.insertMany(users);
+        // Post.insertMany(posts);
     })
     .catch((error) => console.log(error.message));
