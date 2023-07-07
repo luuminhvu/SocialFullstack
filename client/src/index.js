0e99239b42ce629a189a85eb6980406fc71d1f5e
Luu Vu <vu7178@gmail.com>
2023-07-07 23:27:59 +0700

    Navbar Component

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import authReducer from './state';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
-import storage from 'react-redux/lib/storage';
+import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 const persistConfig = {
     key: 'root',
