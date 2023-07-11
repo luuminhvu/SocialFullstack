cd1b634951fb60591c8a1f5a204d6aca9bc00f91
Luu Vu <vu7178@gmail.com>
2023-07-11 22:44:15 +0700

    fix bug

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,9 +1,9 @@
 import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
 import Form from './form';
+
 const LoginPage = () => {
     const theme = useTheme();
-    const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
-
+    const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
     return (
         <Box>
             <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
@@ -19,7 +19,7 @@ const LoginPage = () => {
                 borderRadius="1.5rem"
                 backgroundColor={theme.palette.background.alt}
             >
-                <Typography textAlign="center" fontWeight="500" variant="h5" sx={{ mb: '1.5rem' }}>
+                <Typography fontWeight="500" variant="h5" sx={{ mb: '1.5rem' }}>
                     Welcome to Facebook Login , the new Facebook application for Facebook
                 </Typography>
                 <Form />
@@ -27,4 +27,5 @@ const LoginPage = () => {
         </Box>
     );
 };
+
 export default LoginPage;
--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -6,41 +6,45 @@ import {
     Twitter,
     LinkedIn,
 } from '@mui/icons-material';
-import { Box, Button, Divider, Grid, Typography, useTheme } from '@mui/material';
+import { Box, Typography, Divider, useTheme } from '@mui/material';
 import UserImage from 'components/UserImage';
 import FlexBetween from 'components/FlexBetween';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import WidgetWrapper from 'components/WidgetWrapper';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const UserWidget = ({ userId, picturePath }) => {
     const [user, setUser] = useState(null);
-    const navigate = useNavigate();
     const { palette } = useTheme();
+    const navigate = useNavigate();
     const token = useSelector((state) => state.token);
     const dark = palette.neutral.dark;
     const medium = palette.neutral.medium;
-    const main = palette.primary.main;
+    const main = palette.neutral.main;
 
     const getUser = async () => {
         const response = await fetch(`http://localhost:3001/users/${userId}`, {
             method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
+            headers: { Authorization: `Bearer ${token}` },
         });
         const data = await response.json();
         setUser(data);
     };
+
     useEffect(() => {
         getUser();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-    if (!user) return null;
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    if (!user) {
+        return null;
+    }
+
     const { firstName, lastName, location, occupation, viewedProfile, impressions, friends } = user;
+
     return (
         <WidgetWrapper>
+            {/* FIRST ROW */}
             <FlexBetween gap="0.5rem" pb="1.1rem" onClick={() => navigate(`/profile/${userId}`)}>
                 <FlexBetween gap="1rem">
                     <UserImage image={picturePath} />
@@ -56,7 +60,7 @@ const UserWidget = ({ userId, picturePath }) => {
                                 },
                             }}
                         >
-                            {`${firstName} ${lastName}`}
+                            {firstName} {lastName}
                         </Typography>
                         <Typography color={medium}>{friends.length} friends</Typography>
                     </Box>
@@ -66,6 +70,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
             <Divider />
 
+            {/* SECOND ROW */}
             <Box p="1rem 0">
                 <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
                     <LocationOnOutlined fontSize="large" sx={{ color: main }} />
@@ -77,10 +82,15 @@ const UserWidget = ({ userId, picturePath }) => {
                 </Box>
             </Box>
 
+            <Divider />
+
+            {/* THIRD ROW */}
             <Box p="1rem 0">
                 <FlexBetween mb="0.5rem">
                     <Typography color={medium}>Who's viewed your profile</Typography>
-                    <Typography color={main}>{viewedProfile}</Typography>
+                    <Typography color={main} fontWeight="500">
+                        {viewedProfile}
+                    </Typography>
                 </FlexBetween>
                 <FlexBetween>
                     <Typography color={medium}>Impressions of your post</Typography>
@@ -90,8 +100,11 @@ const UserWidget = ({ userId, picturePath }) => {
                 </FlexBetween>
             </Box>
 
+            <Divider />
+
+            {/* FOURTH ROW */}
             <Box p="1rem 0">
-                <Typography color={main} mb="1rem" fontSize="1rem" fontWeight="500">
+                <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
                     Social Profiles
                 </Typography>
 
@@ -107,6 +120,7 @@ const UserWidget = ({ userId, picturePath }) => {
                     </FlexBetween>
                     <EditOutlined sx={{ color: main }} />
                 </FlexBetween>
+
                 <FlexBetween gap="1rem">
                     <FlexBetween gap="1rem">
                         <LinkedIn sx={{ color: medium }} />
@@ -123,4 +137,5 @@ const UserWidget = ({ userId, picturePath }) => {
         </WidgetWrapper>
     );
 };
+
 export default UserWidget;
